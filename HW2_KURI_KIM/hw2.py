17657933873e729e4b8c1d30d1be4a6bafd3340b
Kuri Kim <sch.kurikim@gmail.com>
2023-04-04 10:58:18 +0900

    hw2 folder

---
+++ HW2_KURI_KIM/hw2.py
@@ -0,0 +1,24 @@
+str = 'My name is Kuri Kim'
+print(str)
+
+#01
+print(len(str))
+#02
+print(str * 10)
+#03
+print(str[0])
+#04
+print(str[0:4])
+#05
+print(str[-4:])
+
+#06
+print(str[::-1])
+#07
+print(str[1:-1])
+#08
+print(str.upper())
+#09
+print(str.lower())
+#10
+print(str.replace('a', 'e'))
